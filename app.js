c966155860e9091ec464d6f8f6a9057534783a40
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-18 23:24:46 +0600

    newly added

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ document.addEventListener('click', () => {
     console.log(`Clicked an even number of times: ${clickCount}`);
   }
 });
+
+
+
+document.addEventListener('keypress', (event) => {
+  const key = event.key;
+  if (!isNaN(key) && Number(key) % 2 === 0) {
+    console.log(`You pressed an even number key: ${key}`);
+  }
+});
+
+
+
+
+let count = 0;
+document.addEventListener('click', () => {
+  count++;
+  if (count % 2 === 0) console.log('Even click:', count);
+});
