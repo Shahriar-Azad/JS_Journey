1673c707f81843ac23c0e52aecbfd23556bc22ce
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-18 23:25:49 +0600

    added last

--- app.js
+++ app.js
@@ -35,3 +35,25 @@ document.addEventListener('keydown', (e) => {
     console.log('Even number key pressed:', key);
   }
 });
+
+
+
+
+// 3. Mouseover event on even-indexed divs (0-based)
+const divs = document.querySelectorAll('div');
+divs.forEach((div, i) => {
+  if ((i + 1) % 2 === 0) {
+    div.addEventListener('mouseover', () => {
+      console.log('Hovered on even div index:', i);
+    });
+  }
+});
+
+
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  if (scrollY % 2 === 0) {
+    console.log('Even scroll position:', scrollY);
+  }
+});
