6b50b5dc31e3e00405f7ac26cf2bcddf0d07a08f
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-19 22:14:04 +0600

    function added

---
+++ function/func.js
@@ -0,0 +1,15 @@
+function add(x,y){
+    return x+y
+}
+function subtract(x,y){
+    return x-y
+}
+function multiply(x,y){
+    return x*y
+}
+function divide(x,y){
+    return Math.floor(x/y)
+}
+
+
+console.log(divide(5,2))
