a1a1e1c0e47b87434ec1abff2a53d25c1c981480
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-20 23:04:41 +0600

    newly added

--- function/func.js
+++ function/func.js
@@ -15,25 +15,54 @@
 // console.log(divide(5,2))
 
 
-function isEven(number){
-    return number % 2 === 0 ? true : false;
-}
+// function isEven(number){
+//     return number % 2 === 0 ? true : false;
+// }
 
-console.log(isEven(10))
-
-
-
-
-function isValidEmail(mail){
-    if(mail.includes("@")){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+// console.log(isEven(10))
 
 
 
-console.log(isValidEmail("bro123@gmail.com"));
 
+// function isValidEmail(mail){
+//     if(mail.includes("@")){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+
+
+// console.log(isValidEmail("bro123@gmail.com"));
+
+
+// function isValidEmail(email){
+//     return email.includes("@") ? true : false
+// }
+
+
+// console.log(isValidEmail("bro123@gmail.com"))
+
+
+// just to commit on github----->
+
+let clickCount = 0;
+document.addEventListener('click', () => {
+  clickCount++;
+  if (clickCount % 2 === 0) {
+    console.log(`Clicked an even number of times: ${clickCount}`);
+  }
+});
+
+
+
+
+
+document.addEventListener('keypress', (event) => {
+  const key = event.key;
+  if (!isNaN(key) && Number(key) % 2 === 0) {
+    console.log(`You pressed an even number key: ${key}`);
+  }
+});
