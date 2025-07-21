19db395613600ebad4be0e2a8f0c1ca5b96d387b
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-21 22:42:49 +0600

    problem 2

--- problem_solving/problem.js
+++ problem_solving/problem.js
@@ -1,18 +1,40 @@
-function palindrome(str){
-    let reversed = "";
-    for(let i = str.length - 1; i >= 0; i-- ){
-        reversed += str[i];
+// Problem-01:
+
+// function palindrome(str){
+//     let reversed = "";
+//     for(let i = str.length - 1; i >= 0; i-- ){
+//         reversed += str[i];
         
 
-    }
+//     }
 
-    if(str === reversed){
-        return true;
-    }
-    else {
-        return false;
-    }
+//     if(str === reversed){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
 
+// }
+
+// console.log(palindrome("racecar"));
+
+
+
+
+// Problem-02:
+
+
+function debounce(func, delay) {
+  let timeoutId;
+
+  return function(...args) {
+    // Clear the previous timer
+    clearTimeout(timeoutId);
+
+    // Set a new timer
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
 }
-
-console.log(palindrome("racecar"));
