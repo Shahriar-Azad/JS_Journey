3514a8c16ec3363ccb5d008ae771de3e53557947
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-26 23:31:49 +0600

    done again

--- ungabunga/ungabunga.js
+++ ungabunga/ungabunga.js
@@ -1,6 +1,11 @@
-const emojis = ["😎", "🚀", "🔥", "✨", "🎉", "🍕", "🐱‍👤", "💻", "🧠"];
-function getRandomEmoji() {
-  return emojis[Math.floor(Math.random() * emojis.length)];
-}
+// const emojis = ["😎", "🚀", "🔥", "✨", "🎉", "🍕", "🐱‍👤", "💻", "🧠"];
+// function getRandomEmoji() {
+//   return emojis[Math.floor(Math.random() * emojis.length)];
+// }
 
-console.log("Here's a random emoji for you:", getRandomEmoji());
+// console.log("Here's a random emoji for you:", getRandomEmoji());
+
+
+const numbers = [1, 2, 3];
+const doubled = numbers.customMap(num => num * 2);
+console.log(doubled); // [2, 4, 6]
