1be98355e0e137d2b2824fe7ce76ca850b2343e0
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-26 23:32:26 +0600

    done all

--- ungabunga/ungabunga.js
+++ ungabunga/ungabunga.js
@@ -6,6 +6,15 @@
 // console.log("Here's a random emoji for you:", getRandomEmoji());
 
 
-const numbers = [1, 2, 3];
-const doubled = numbers.customMap(num => num * 2);
-console.log(doubled); // [2, 4, 6]
+// const numbers = [1, 2, 3];
+// const doubled = numbers.customMap(num => num * 2);
+// console.log(doubled); // [2, 4, 6]
+
+
+Array.prototype.customMap = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
