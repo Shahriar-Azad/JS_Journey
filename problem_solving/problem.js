5eb7e42eb8349d7132f993559dc5b1be6579f6ed
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-29 23:53:36 +0600

    done2

--- problem_solving/problem.js
+++ problem_solving/problem.js
@@ -311,8 +311,14 @@
 
 
 
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(10,20));
+
+function sub(a,b){
+    return a-b;
 }
 
-console.log(add(10,20));
+console.log(sub(20,10));
