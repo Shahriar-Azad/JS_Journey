d15dbcdcbdc0283627feb4f59de34b11789ca4e0
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-01 23:57:53 +0600

    added

--- DOM/mod.js
+++ DOM/mod.js
@@ -24,6 +24,11 @@
 
 
 
-const deepClone = obj => JSON.parse(JSON.stringify(obj));
-const original = {a: 1, b: {c: 2}};
-const cloned = deepClone(original);
+// const deepClone = obj => JSON.parse(JSON.stringify(obj));
+// const original = {a: 1, b: {c: 2}};
+// const cloned = deepClone(original);
+
+
+
+const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)];
+console.log(getRandomItem(['apple', 'banana', 'cherry'])); 
