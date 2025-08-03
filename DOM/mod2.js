ebec4a6c8d6291620f5593a11f6a3f11dfc680db
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-03 23:47:41 +0600

    new

--- DOM/mod2.js
+++ DOM/mod2.js
@@ -36,7 +36,7 @@
 
 
 
-const toTitleCase = str => str.replace(/\w\S*/g, txt => 
-  txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-);
-console.log(toTitleCase('hello world'));
+// const toTitleCase = str => str.replace(/\w\S*/g, txt => 
+//   txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+// );
+// console.log(toTitleCase('hello world'));
