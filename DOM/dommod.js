d0533619c7783bc48ca1946e6548555d17a6bf16
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-08-05 00:29:57 +0600

    added

--- DOM/dommod.js
+++ DOM/dommod.js
@@ -15,10 +15,19 @@
 
 
 
-const debounce = (fn, delay) => {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
-  };
-};
+// const debounce = (fn, delay) => {
+//   let timer;
+//   return (...args) => {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => fn(...args), delay);
+//   };
+// };
+
+
+
+
+
+
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+const original = {a: 1, b: {c: 2}};
+const cloned = deepClone(original);
